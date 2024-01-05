fc7aa6795907f733a3f6467edaa705c11d618389
Andrew Fasano <fasano@mit.edu>
2024-01-05 18:01:35 -0500

    Build scripts: support optional target argument, rename scripts.

--- docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TARGETLIST="armel mipseb mipsel"
-
-set -eu
-
-docker build -t pandare/kernel_builder .
-docker run --rm -v $PWD:/app pandare/kernel_builder bash /app/build.sh $TARGETLIST
