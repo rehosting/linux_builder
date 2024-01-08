937e8d3a8294432c7402f3846f85c51c5bd5eef8
Andrew Fasano <fasano@mit.edu>
2024-01-08 17:06:25 -0500

    Add linter for kernel configs in _in_container_build.
    Run with ./build.sh configonly [target list]

--- _in_container_build.sh
+++ _in_container_build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 # We want to build linux for each of our targets using the config files. Linux is in /app/linux
 # while our configs are at config.[arch]. We need to set the ARCH and CROSS_COMPILE variables
 # and put the binaries in /app/binaries
 
-TARGET_LIST=${1:-"armel mipsel mipseb mips64eb"}
+# If our first argument is configonly we'll only update the defconfigs
+CONFIGONLY=false
+if [ $# -gt 0 ] && [ "$1" == "configonly" ]; then
+    CONFIGONLY=true
+    shift
+fi
+
+# Arguments are a list of architectures.
+# If none are set, we'll use our defaults
+if [ $# -eq 0 ]; then
+    TARGET_LIST="armel mipseb mipsel mips64eb"
+else
+    TARGET_LIST=$@
+fi
+
+echo "Configonly: $CONFIGONLY"
+echo "Target_list: $TARGET_LIST"
+
+# Set this to update defconfigs instead of building kernel
+
 mkdir -p /kernels
 
 get_cc() {
@@ -52,24 +71,34 @@ for TARGET in $TARGET_LIST; do
     # Actually build
     echo "Building kernel for $TARGET"
     make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ olddefconfig
-    make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ $BUILD_TARGETS -j$(nproc)
 
-    # Copy out zImage (if present) and vmlinux (always)
-    if [ -f "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" ]; then
-        cp "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" /kernels/zImage.${TARGET}
+    # If updating configs, lint them with kernel first! This sorts, removes default options and duplicates.
+    if $CONFIGONLY; then
+      echo "Updating $TARGET config in place"
+      make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ savedefconfig
+      cp /tmp/build/${TARGET}/defconfig /app/config.${TARGET}
+      echo "Finished update for config.${TARGET}"
+    else
+      make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ $BUILD_TARGETS -j$(nproc)
+
+      # Copy out zImage (if present) and vmlinux (always)
+      if [ -f "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" ]; then
+          cp "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" /kernels/zImage.${TARGET}
+      fi
+      cp "/tmp/build/${TARGET}/vmlinux" /kernels/vmlinux.${TARGET}
+
+      # Generate OSI profile
+      echo "[${TARGET}]" >> /kernels/osi.config
+      /panda/panda/plugins/osi_linux/utils/kernelinfo_gdb/run.sh \
+        /kernels/vmlinux.${TARGET} /tmp/panda_profile.${TARGET}
+      cat /tmp/panda_profile.${TARGET} >> /kernels/osi.config
+
+        /dwarf2json/dwarf2json linux --elf /kernels/vmlinux.${TARGET} \
+        | xz - > /kernels/vmlinux.${TARGET}.json.xz
     fi
-    cp "/tmp/build/${TARGET}/vmlinux" /kernels/vmlinux.${TARGET}
-
-	# Generate OSI profile
-	echo "[${TARGET}]" >> /kernels/osi.config
-	/panda/panda/plugins/osi_linux/utils/kernelinfo_gdb/run.sh \
-		/kernels/vmlinux.${TARGET} /tmp/panda_profile.${TARGET}
-	cat /tmp/panda_profile.${TARGET} >> /kernels/osi.config
-
-    /dwarf2json/dwarf2json linux --elf /kernels/vmlinux.${TARGET} \
-		| xz - > /kernels/vmlinux.${TARGET}.json.xz
-
 done
 
-echo "Built by linux_builder on $(date)" > /kernels/README.txt
-tar cvfz /app/kernels-latest.tar.gz /kernels
+if ! $CONFIGONLY; then
+  echo "Built by linux_builder on $(date)" > /kernels/README.txt
+  tar cvfz /app/kernels-latest.tar.gz /kernels
+fi
