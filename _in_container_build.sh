ba161a3542a534c3811a813bf5ba9daf6fb10d87
Benjamin Levy <benjamin.levy@ll.mit.edu>
2024-01-17 12:42:23 -0500

    Improve build.sh CLI and support multiple kernel versions

--- _in_container_build.sh
+++ _in_container_build.sh
@@ -2,31 +2,20 @@
 
 set -eu
 
-# We want to build linux for each of our targets using the config files. Linux is in /app/linux
+# We want to build linux for each of our targets and versions using the config files. Linux is in /app/linux/[version]
 # while our configs are at config.[arch]. We need to set the ARCH and CROSS_COMPILE variables
 # and put the binaries in /app/binaries
 
-# If our first argument is configonly we'll only update the defconfigs
-CONFIGONLY=false
-if [ $# -gt 0 ] && [ "$1" == "configonly" ]; then
-    CONFIGONLY=true
-    shift
-fi
+# Get options from build.sh
+CONFIG_ONLY="$1"
+VERSIONS="$2"
+TARGETS="$3"
 
-# Arguments are a list of architectures.
-# If none are set, we'll use our defaults
-if [ $# -eq 0 ]; then
-    TARGET_LIST="armel mipseb mipsel mips64eb"
-else
-    TARGET_LIST=$@
-fi
-
-echo "Configonly: $CONFIGONLY"
-echo "Target_list: $TARGET_LIST"
+echo "Config only: $CONFIG_ONLY"
+echo "Versions: $VERSIONS"
+echo "Targets: $TARGETS"
 
 # Set this to update defconfigs instead of building kernel
-
-mkdir -p /kernels
 
 get_cc() {
     local arch=$1
@@ -47,7 +36,8 @@ get_cc() {
     echo "/opt/cross/${arch}-linux-musl${abi}/bin/${arch}-linux-musl${abi}-"
 }
 
-for TARGET in $TARGET_LIST; do
+for VERSION in $VERSIONS; do
+for TARGET in $TARGETS; do
     BUILD_TARGETS="vmlinux"
     if [ $TARGET == "armel" ]; then
         BUILD_TARGETS="vmlinux zImage"
@@ -70,35 +60,35 @@ for TARGET in $TARGET_LIST; do
 
     # Actually build
     echo "Building kernel for $TARGET"
-    make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ olddefconfig
+    make -C /app/linux/$VERSION ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ olddefconfig
 
     # If updating configs, lint them with kernel first! This sorts, removes default options and duplicates.
-    if $CONFIGONLY; then
+    if $CONFIG_ONLY; then
       echo "Updating $TARGET config in place"
-      make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ savedefconfig
+      make -C /app/linux/$VERSION ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ savedefconfig
       cp /tmp/build/${TARGET}/defconfig /app/config.${TARGET}
       echo "Finished update for config.${TARGET}"
     else
-      make -C /app/linux ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ $BUILD_TARGETS -j$(nproc)
+      make -C /app/linux/$VERSION ARCH=${short_arch} CROSS_COMPILE=$(get_cc $TARGET) O=/tmp/build/${TARGET}/ $BUILD_TARGETS -j$(nproc)
+
+      mkdir -p /kernels/$VERSION
 
       # Copy out zImage (if present) and vmlinux (always)
       if [ -f "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" ]; then
-          cp "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" /kernels/zImage.${TARGET}
+          cp "/tmp/build/${TARGET}/arch/${short_arch}/boot/zImage" /kernels/$VERSION/zImage.${TARGET}
       fi
-      cp "/tmp/build/${TARGET}/vmlinux" /kernels/vmlinux.${TARGET}
+      cp "/tmp/build/${TARGET}/vmlinux" /kernels/$VERSION/vmlinux.${TARGET}
 
       # Generate OSI profile
-      echo "[${TARGET}]" >> /kernels/osi.config
+      echo "[${TARGET}]" >> /kernels/$VERSION/osi.config
       /panda/panda/plugins/osi_linux/utils/kernelinfo_gdb/run.sh \
-        /kernels/vmlinux.${TARGET} /tmp/panda_profile.${TARGET}
-      cat /tmp/panda_profile.${TARGET} >> /kernels/osi.config
-
-        #/bin/dwarf2json linux --elf /kernels/vmlinux.${TARGET} \
-        #| xz - > /kernels/vmlinux.${TARGET}.json.xz
+        /kernels/$VERSION/vmlinux.${TARGET} /tmp/panda_profile.${TARGET}
+      cat /tmp/panda_profile.${TARGET} >> /kernels/$VERSION/osi.config
     fi
 done
+done
 
-if ! $CONFIGONLY; then
+if ! $CONFIG_ONLY; then
   echo "Built by linux_builder on $(date)" > /kernels/README.txt
   tar cvfz /app/kernels-latest.tar.gz /kernels
 fi
